e1b412169f55cd5ee033c0cae1ac09c410ab20db
hayabusa1gou <s3029524@outlook.jp>
2022-08-21 15:40:10 +0900

    昨日のエラー修正。

--- sample_app/app/controllers/likes_controller.rb
+++ sample_app/app/controllers/likes_controller.rb
@@ -2,16 +2,16 @@ class LikesController < ApplicationController
 
     def likes_create
         @post=Post.find_by(id:params[:id])
-        @like=Review.new(user_id:@current_user.id,post_id:params[:id])
+        @like=Like.new(user_id:@current_user.id,post_id:params[:id])
         @like.save
-        redirect_to("/posts/#{@post.id}")
+        redirect_to("/posts/index")
     end
 
     def likes_destroy
         @post=Post.find_by(id:params[:id])
-        @like=Review.find_by(user_id:@current_user.id,post_id:params[:id])
+        @like=Like.find_by(user_id:@current_user.id,post_id:params[:id])
         @like.destroy
-        redirect_to("/posts/#{@post.id}")
+        redirect_to("/posts/index")
 
     end
 
